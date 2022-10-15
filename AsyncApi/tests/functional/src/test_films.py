b965c0889b3a5b87179cfbc53b4c8b79fbff1a46
bekishev04 <bekishev04@yandex.ru>
2022-10-15 15:33:02 +0300

    films redis

--- AsyncApi/tests/functional/src/test_films.py
+++ AsyncApi/tests/functional/src/test_films.py
@@ -28,6 +28,24 @@ async def test_search(es_write_data, make_get_request):
     assert len(response.body["items"]) == 50
 
 
+async def test_cache_genre(make_get_request):
+
+    uuid_key = uuid.uuid4()
+    data = create_data(name="Test", id=uuid_key)
+
+    es_client = AsyncElasticsearch(hosts=test_settings.elastic_uri, verify_certs=False)
+
+    await helpers.async_bulk(es_client, generate_doc([data], "movies"))
+
+    response_first = await make_get_request(f"api/v1/film-work/movie/{uuid_key}")
+    assert response_first.status == 200
+
+    await helpers.async_bulk(es_client, delete_doc([data], "movies"))
+
+    response_second = await make_get_request(f"api/v1/film-work/movie/{uuid_key}")
+    assert response_second.status == 200
+    assert response_first.body == response_second.body
+
 
 async def test_not_found(make_get_request):
     uuid_key = uuid.uuid4()
